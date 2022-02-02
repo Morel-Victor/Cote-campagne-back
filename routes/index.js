e9f56d43a4fed796752f949fb7b4b98589d0a9f4
Morel Victor <morel.victor1@gmail.com>
2022-02-02 15:41:52 +0100

    Add all pages and default routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const carouselRouter = require('./carousel');
-// const homeRoutes = require('./home');
-// const sheduleRoutes = require('./shedule');
-// const galleryRoutes = require('./gallery');
+const homeRoutes = require('./home');
+const sheduleRoutes = require('./shedule');
+const galleryRoutes = require('./gallery');
 
 const setupRoutes = (app) => {
   app.use('/carousel', carouselRouter);
-  // app.use('/home', homeRoutes);
-  // app.use('/shedule', sheduleRoutes);
-  // app.use('/gallerie', galleryRoutes);
+  app.use('/home', homeRoutes);
+  app.use('/shedule', sheduleRoutes);
+  app.use('/gallerie', galleryRoutes);
 };
 
 module.exports = {
---
+++ routes/shedule.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const router = express.Router();
+const { db } = require('../db-config');
+
+router.get('/', async (req, res) => {
+  try {
+    const [img] = await db.query(`
+    SELECT * FROM Shedule
+  `);
+    res.json(img).status(201);
+  } catch (err) {
+    res.status(404);
+    console.warn(err);
+  }
+});
+
+module.exports = router;
